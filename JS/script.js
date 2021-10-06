b2380fd4e8c0e073cb823ed5550654034e496127
atlestate1 <89618603+atlestate1@users.noreply.github.com>
2021-10-06 18:26:54 -0400

    creating array for Pokemon
    Creating the array for the Pokemon app by adding the characters and their respective attributes.

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,8 @@
-alert('Hello World');
-
-//defining variables//
-const favoriteFood = 'Salmon';
-document.write(favoriteFood);
+let pokemonlist = [
+    {name: 'Charmander', height: 0.6, type: ['fire']},
+    {name: 'Beedrill', height: 1.0, type: ['bug','poison']},
+    {name: 'Pidgeotto', height: 1.1, type: ['flying', 'normal']},
+    {name: 'Nidoran', height: 0.4, type: ['poison']},
+    {name: 'Arcanine', height: 1.9, type: ['fire']},
+    {name: 'Ho-oh', height: 3.8, type: ['fire', 'flying']}
+]
