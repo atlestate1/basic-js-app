47af6459c3a523fb72f6e153630d2e9718047686
atlestate1 <89618603+atlestate1@users.noreply.github.com>
2021-10-06 23:14:23 -0400

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,8 @@
-let pokemonlist = [
-    {name: 'Charmander', height: 0.6, type: ['fire']},
-    {name: 'Beedrill', height: 1.0, type: ['bug','poison']},
-    {name: 'Pidgeotto', height: 1.1, type: ['flying', 'normal']},
-    {name: 'Nidoran', height: 0.4, type: ['poison']},
-    {name: 'Arcanine', height: 1.9, type: ['fire']},
-    {name: 'Ho-oh', height: 3.8, type: ['fire', 'flying']}
+const pokemonList = [
+    {name: 'Charmander', height: 0.6, types: ['fire']},
+    {name: 'Beedrill', height: 1.0, types: ['bug','poison']},
+    {name: 'Pidgeotto', height: 1.1, types: ['flying', 'normal']},
+    {name: 'Nidoran', height: 0.4, types: ['poison']},
+    {name: 'Arcanine', height: 1.9, types: ['fire']},
+    {name: 'Ho-oh', height: 3.8, types: ['fire', 'flying']}
 ]
