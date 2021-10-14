cb9912f93e44c531702bcdc887e0c878f027f18b
atlestate1 <89618603+atlestate1@users.noreply.github.com>
2021-10-14 07:08:03 -0400

    Add loop to identify which characters are huge based on height

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,30 @@
-alert('Hello World');
+//Create list of Pokemon characters with height and type attributes//
+const pokemonList = [
+    {name: 'Charmander', height: 0.6, types: ['fire']},
+    {name: 'Beedrill', height: 1.0, types: ['bug','poison']},
+    {name: 'Pidgeotto', height: 1.1, types: ['flying', 'normal']},
+    {name: 'Nidoran', height: 0.4, types: ['poison']},
+    {name: 'Arcanina', height: 1.9, types: ['fire']},
+    {name: 'Ho-oh', height: 3.8, types: ['fire', 'flying']}
+];
 
-//defining variables//
-const favoriteFood = 'Salmon';
-document.write(favoriteFood);
+
+//for loop to iterate over each element in array//
+
+for (let i = 0; i < pokemonList.length; i++) {
+  console.log(pokemonList[i].name, pokemonList[i].height,
+    pokemonList[i].types);
+//write name and height of Pokemon on screen//
+  document.write(pokemonList[i].name + " is ", pokemonList[i].height +
+    " in height.", '<br>');
+};
+
+
+/*Create for loop to identify which Pokemon characters are larger than size
+in for loop and attach a message stating the character is big*/
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 1.5)
+  document.write(pokemonList[i].name + " ", pokemonList[i].height +
+    " Wow, that's big!!", '<br>');
+};
