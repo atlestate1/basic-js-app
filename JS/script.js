20086c795aeebf2d1283366b1a1b20f58c707b83
atlestate1 <89618603+atlestate1@users.noreply.github.com>
2021-10-13 23:06:33 -0400

    Use for loop through list
    Print specific items to computer

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,20 @@
-alert('Hello World');
+//Create list of Pokemon characters with height and type attributes//
+const pokemonList = [
+    {name: 'Charmander', height: 0.6, types: ['fire']},
+    {name: 'Beedrill', height: 1.0, types: ['bug','poison']},
+    {name: 'Pidgeotto', height: 1.1, types: ['flying', 'normal']},
+    {name: 'Nidoran', height: 0.4, types: ['poison']},
+    {name: 'Arcanina', height: 1.9, types: ['fire']},
+    {name: 'Ho-oh', height: 3.8, types: ['fire', 'flying']}
+];
 
-//defining variables//
-const favoriteFood = 'Salmon';
-document.write(favoriteFood);
+
+//for loop to iterate over each element in array//
+
+for (const i = 0; i < pokemonList.length; i++) {
+  console.log(pokemonList[i].name, pokemonList[i].height,
+    pokemonList[i].types);
+//write name and height of Pokemon on screen//
+  document.write(pokemonList[i].name + " is ", pokemonList[i].height +
+    " in height".);
+};
