a20b6705e4779ff0ee5ce90ecea9bf8e69e6dc78
atlestate1 <89618603+atlestate1@users.noreply.github.com>
2021-10-20 05:46:51 -0400

    update code
    updated minor changes in code

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,11 @@
-
+const pokemonList = [
+    {name: 'Charmander', height: 0.6, types: ['fire']},
+    {name: 'Beedrill', height: 1.0, types: ['bug','poison']},
+    {name: 'Pidgeotto', height: 1.1, types: ['flying', 'normal']},
+    {name: 'Nidoran', height: 0.4, types: ['poison']},
+    {name: 'Arcanine', height: 1.9, types: ['fire']},
+    {name: 'Ho-oh', height: 3.8, types: ['fire', 'flying']}
+]
 
 //Use foreach loop instead of for loop to iterate over Pokemon List//
 
