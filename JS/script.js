cfbc1d041bda67eee61a8f3bec85b8aaf6ce8f7e
atlestate1 <89618603+atlestate1@users.noreply.github.com>
2021-10-25 14:10:46 -0400

    update js file
    copy code from branch 1.5

--- JS/script.js
+++ JS/script.js
@@ -1,39 +1,9 @@
-const pokemonList = [
-    {name: 'Charmander', height: 0.6, types: ['fire']},
-    {name: 'Beedrill', height: 1.0, types: ['bug','poison']},
-    {name: 'Pidgeotto', height: 1.1, types: ['flying', 'normal']},
-    {name: 'Nidoran', height: 0.4, types: ['poison']},
-    {name: 'Arcanine', height: 1.9, types: ['fire']},
-    {name: 'Ho-oh', height: 3.8, types: ['fire', 'flying']}
-]
-
-//Use foreach loop instead of for loop to iterate over Pokemon List//
-
-function myPokemon(pokemon) {
-  document.write(pokemon.name + ' is ' + pokemon.height + ' inches tall ' +
-  'and special powers are '  + pokemon.types + '.', '<br>');
-}
-pokemonList.forEach(myPokemon);
-
-
-/*Create for loop to identify which Pokemon characters are larger than size
-in for loop and attach a message stating the character is big*/
-
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 1.5 && pokemonList[i].height < 2.5)
-  document.write(pokemonList[i].name + " ", pokemonList[i].height +
-    " Wow, that's big!!", '<br>')
-  if (pokemonList[i].height > 2.5)
-  document.write(pokemonList[i].name + " ", pokemonList[i].height +
-    " Wow, that's HUGE!!", '<br>');
-};
-
 /*Create IIFE to avoid accidentally accessing the global state and create new
 pokemon repository variable to hold what IIFE will return and assign IIFE
 to that variable.*/
 
-let pokemonRepository = (function () {
-  let pokemonList = [
+const pokemonRepository = (function () {
+  const pokemonList = [
     {
       name: 'Charmander',
       height: 0.6,
@@ -79,5 +49,13 @@ let pokemonRepository = (function () {
   };
 })();
 
-console.log( pokemonRepository.getAll() );
 pokemonRepository.add({ name: 'Ponyta', height: 1.0, types: ['fire'] });
+const pokemonList = pokemonRepository.getAll();
+
+//Use foreach loop instead of for loop to iterate over Pokemon List//
+
+function myPokemon(pokemon) {
+  document.write(pokemon.name + ' is ' + pokemon.height + ' inches tall ' +
+  'and special powers are '  + pokemon.types + '.', '<br>');
+}
+pokemonList.forEach(myPokemon);
