00fa68ab83558f3875d8d47aa7df465414e0f9c6
atlestate1 <89618603+atlestate1@users.noreply.github.com>
2021-10-26 17:48:56 -0400

    fetch details
    added fetch details from the pokemon api, specifically targeting the name, height and type

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,89 @@
-alert('Hello World');
+/*Create IIFE to avoid accidentally accessing the global state and create new
+pokemon repository variable to hold what IIFE will return and assign IIFE
+to that variable.*/
 
-//defining variables//
-const favoriteFood = 'Salmon';
-document.write(favoriteFood);
+const pokemonRepository = (function () {
+  const pokemonList = [];
+  let apiUrl = 'https//pokeapi.co/api/v2/pokemon/?limit=150';
+
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+/*Add function to call at the end in the loop function.  This function creates
+the buttons for each pokemon*/
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+
+//add listener function to create action once user clicks button//
+    button.addEventListener('click', function(event) {
+      showDetails(pokemon);
+    });
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon.name);
+  }
+
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
+  });
+}
+  return {
+    getAll: getAll,
+    add: add,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
+  };
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Ponyta', height: 1.0, types: ['fire'] });
+
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
+});
